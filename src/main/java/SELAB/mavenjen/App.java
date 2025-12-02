fbe76a788de8c3ca10264ce03d53af5ab1f4a26d
kpavithrachowdary <chowpavithra48@gmail.com>
2025-12-03 00:26:38 +0530

    Change greeting from 'Hello Kmit' to 'Hello'

--- src/main/java/SELAB/mavenjen/App.java
+++ src/main/java/SELAB/mavenjen/App.java
@@ -5,6 +5,6 @@ package SELAB.mavenjen;
  */
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello Kmit");
+        System.out.println("Hello");
     }
 }
